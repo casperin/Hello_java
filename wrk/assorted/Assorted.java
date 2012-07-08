8136a5f03aa79a7bb6e4c14eb3b0970778a8003f
Gorm Casper <g@g81.dk>
2012-07-08 19:33:34 +0200

    Added assorted

---
+++ wrk/assorted/Assorted.java
@@ -0,0 +1,23 @@
+
+public class Assorted {
+  public static void main (String args[]) {
+
+    System.out.println("\nAssorted stuff! Look in file(s)\n");
+
+
+
+    //
+    // For/in loop
+    //
+
+    // List
+    int[] l = {11,12,13,14,15,16};
+
+    // For each "num" (of type "int") in the list "l", do...
+    for (int num : l) {
+      System.out.println(num);
+    }
+  
+  
+  }
+}
